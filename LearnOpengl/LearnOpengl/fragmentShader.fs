d0211c59ecbd2637d27c10837893bb7d76af2291
Diyvansh Gupta <divyanshgupta.dg123@gmail.com>
2024-06-15 15:53:13 +0530

    Transformation in Opengl

--- LearnOpengl/LearnOpengl/fragmentShader.fs
+++ LearnOpengl/LearnOpengl/fragmentShader.fs
@@ -1,8 +1,8 @@
 
+
 #version 330 core
 out vec4 FragColor;
 
-in vec3 ourColor;
 in vec2 TexCoord;
 
 // texture samplers
@@ -11,7 +11,7 @@ uniform sampler2D texture2;
 
 void main()
 {
-
-    FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.2);
+	// linearly interpolate between both textures (80% container, 20% awesomeface)
+	FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.2);
 }
 
